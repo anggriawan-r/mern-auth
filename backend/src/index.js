bff5d8dc0f268691e7955d22bc7108110fdd73f4
Anggriawan Rayzadmiko <anggriawan.net@gmail.com>
2024-08-25 15:36:35 +0700

    build: change folder structure for deploying purpose

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
